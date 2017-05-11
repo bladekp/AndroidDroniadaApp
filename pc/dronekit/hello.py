cdd0d3a6f5aea311647885bf639a26c1f2b4df38
bladekp <bladekp@ee.pw.edu.pl>
2017-05-11 20:04:02 +0200

    pc init

---
+++ pc/dronekit/hello.py
@@ -0,0 +1,20 @@
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+print("Start")
+# Connect to the Vehicle.
+vehicle = connect("127.0.0.1:4550", wait_ready=True)
+
+# Get some vehicle attributes (state)
+print "Get some vehicle attribute values:"
+print " GPS: %s" % vehicle.gps_0
+print " Battery: %s" % vehicle.battery
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Mode: %s" % vehicle.mode.name    # settable
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+# Shut down simulator
+print("Completed")
